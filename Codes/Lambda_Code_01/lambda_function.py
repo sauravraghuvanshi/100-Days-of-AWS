46a5f9b13c519bd3c8ac0ee93b6287f30ac438ed
sauravraghuvanshi <sauravraghuvanshi24@gmail.com>
2021-09-03 19:21:52 +0530

    added lambda function code

---
+++ Codes/Lambda_Code_01/lambda_function.py
@@ -0,0 +1,10 @@
+def lambda_handler(event, context):
+    
+    message = 'Hello {} {}! Keep being awesome!'.format(event['first_name'], event['last_name'])  
+
+    #print to CloudWatch logs
+    print(message)
+
+    return { 
+        'message' : message
+    } 
